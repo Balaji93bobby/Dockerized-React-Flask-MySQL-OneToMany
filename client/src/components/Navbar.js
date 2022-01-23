0e8768303b8e24a09708c3d6dfb292e898332093
Liam Desmond <liamjdesmond84@gmail.com>
2022-01-22 22:31:15 -0600

    Adding Google font

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { navigate } from "@reach/router";
 import { Link } from "@reach/router";
+import '../App.css';
 
 const Navbar = () => {
     
@@ -16,7 +17,7 @@ const Navbar = () => {
             <header className="relative">
             <nav className="bg-steel">
                 <div className="bg-steel  px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-                    <h3 className="text-3xl font-bold bg-tahiti-500 text-silver">Travel Blog</h3>
+                    <h3 className="text-4xl font-bold bg-tahiti-500 text-silver font-head">Travel Blog</h3>
 
                     <div className="container flex flex-wrap justify-between items-center mx-auto">
                         <div className="hidden w-full md:block md:w-auto">
